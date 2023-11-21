8b4752334210918798e685e811bdf80fdbe385d8
xxjay <760409558@qq.com>
2023-11-21 15:45:46 +0800

    feat: add CreateFileIfNotExist

--- util/fs.go
+++ util/fs.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func CreateDirsIfNotExist(dirs []string) error {
@@ -20,4 +21,17 @@ func CreateDirsIfNotExist(dirs []string) error {
 
 func RemoveDir(dir string) error {
 	return os.RemoveAll(dir)
+}
+
+func CreateFileIfNotExist(path string) error {
+	dir := path[:strings.LastIndex(path, "/")]
+	if err := CreateDirsIfNotExist([]string{dir}); err != nil {
+		return err
+	}
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		if _, err := os.Create(path); err != nil {
+			return fmt.Errorf("unable to create file %s, %w", path, err)
+		}
+	}
+	return nil
 }
