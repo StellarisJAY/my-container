ea9dc01812fb14bbae2ebd5487afbc6a95f66007
xxjay <760409558@qq.com>
2023-11-21 15:47:28 +0800

    feat: cmd args

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"github.com/StellarisJAY/my-container/container"
+	"github.com/StellarisJAY/my-container/image"
+	"log"
+	"os"
+)
+
+func main() {
+	cmd := os.Args[1]
+	opts := &container.Options{}
+	var (
+		containerId string
+		imageName   string
+	)
+	fs := flag.FlagSet{}
+	fs.Float64Var(&opts.CpuLimit, "cpu", 1, "Set cpu limit")
+	fs.IntVar(&opts.MemLimit, "mem", 1<<20, "Set memory limit")
+	fs.StringVar(&containerId, "container", "", "Container id")
+	fs.StringVar(&imageName, "image", "", "Image full name")
+	switch cmd {
+	case "run":
+		_ = fs.Parse(os.Args[2:])
+		log.Println(imageName)
+		_ = image.DownloadImageIfNotExist(imageName)
+		container.Run(opts, "", os.Args[2:])
+	case "child-mode":
+		_ = fs.Parse(os.Args[2:])
+		container.Exec(containerId, opts.CpuLimit, opts.MemLimit, os.Args[2:])
+	}
+}
